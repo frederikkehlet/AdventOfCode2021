de96221f5cb0b8f46c3e3829aee1a0698c117c79
Frederik Kehlet <frederik.kehlet@outlook.dk>
2021-12-18 15:14:09 +0100

    day 1 solution

---
+++ Day01/Program.cs
@@ -0,0 +1,27 @@
+﻿string inputFilePath = @"D:\PersonalProjects\AdventOfCode2021\Day01\input.txt";
+string[] numbers = System.IO.File.ReadAllLines(inputFilePath);
+
+// part 1
+int count = 0;
+for (int i = 1; i < numbers.Length; i++) 
+{
+    if (Convert.ToInt32(numbers[i]) > Convert.ToInt32(numbers[i-1])) count++;
+}
+Console.WriteLine($"Part 1 answer: {count}");
+
+// part 2
+count = 0;
+for (int i = 1; i < numbers.Length - 2; i++) 
+{
+    int previousWindowSum = 0;
+    int currentWindowSum = 0;
+    
+    string[] previousWindow = numbers[(i-1)..((i-1) + 3)]; // the last index is non-inclusive
+    string[] currentWindow = numbers[i..(i+3)];
+
+    foreach (var num in previousWindow) previousWindowSum += Convert.ToInt32(num);
+    foreach (var num in currentWindow) currentWindowSum += Convert.ToInt32(num);
+    
+    if (currentWindowSum > previousWindowSum) count++;
+}
+Console.WriteLine($"Part 2 answer: {count}");
